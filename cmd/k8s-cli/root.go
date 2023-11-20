fe7c51b88ecce26087e79244cee74de865fceb5b
Your Name <you@example.com>
2023-11-20 03:26:51 +0000

    completed

--- cmd/k8s-cli/root.go
+++ cmd/k8s-cli/root.go
@@ -2,49 +2,34 @@ package k8scli
 
 import (
 	"fmt"
-	"os"
-	"os/exec"
 
 	"github.com/spf13/cobra"
 )
 
-var rootCmd = &cobra.Command{
-	Use:   "cli tool",
-	Short: "The app executes kubectl command line",
-	Long:  "The app executes kubectl command line for print hello-world and get pod",
-	Run: func(cmd *cobra.Command, args []string) {
-		if len(args) > 1 && args[0] == "kubectl" {
-			switch args[1] {
-			case "get":
-				if len(args) > 2 {
-					execKubectl(args)
-				}
-			case "duydo":
-				fmt.Println("Hello World")
-			default:
-				fmt.Println("command not found")
-			}
-		} else {
-			fmt.Println("incorrect input")
-		}
-	},
-}
-
-func execKubectl(args []string) {
-	cmdArgs := args[1:]
-	cmd := exec.Command("kubectl", cmdArgs...)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stdin
-	err := cmd.Run()
-	if err != nil {
-		fmt.Println("error executing kubectl %v\n", err)
-		os.Exit(1)
-	}
-}
-
 func Execute() {
+	// root command for the plugin
+	rootCmd := &cobra.Command{
+		Use:   "kubectl",
+		Short: "A sample kubectl plugin that prints 'hello-world' when called with 'duydo'",
+	}
+	// funtion handle 'duydo' command
+	handleddoCmd := func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 || args[0] != "duydo" {
+			fmt.Println("invalid argument.")
+			return
+		}
+		fmt.Println("hello world")
+	}
+	// create a subcommand
+	ddoCmd := &cobra.Command{
+		Use:   "duydo",
+		Short: "Print 'hello-world'",
+		Run:   handleddoCmd,
+	}
+	// add subcom to rootcom
+	rootCmd.AddCommand(ddoCmd)
+
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		fmt.Println("Error executing  plugin", err)
 	}
 }
